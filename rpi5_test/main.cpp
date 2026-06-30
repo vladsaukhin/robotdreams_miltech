cecf50087d9d56f481196f7aa4ee3893e50d34a7
vsaukhin <vladsaukhin.official@gmail.com>
2026-06-30 16:06:01 +0000

    Cleanup

--- rpi5_test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <thread>
-
-int main()
-{
-  std::cout << "Hello from Raspberry Pi 5 ARM64\n";
-  std::cout << "CPU threads: " << std::thread::hardware_concurrency() << "\n";
-  return 0;
-}
